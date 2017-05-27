5f280173e8876a2860893117d5688d3f2c2fbcc8
skorponx <davidsoto17011@gmail.com>
2017-05-27 00:13:57 -0500

    recomendador de contenido, falta normalizar

--- recomendadorDeContenido/src/main/scala/RecommendSystem.scala
+++ recomendadorDeContenido/src/main/scala/RecommendSystem.scala
@@ -12,35 +12,40 @@ import org.apache.spark.sql.functions._
 
 object RecommendationSystem {
 
- /** Método encargado de preprocesar el dataframe para el recomendador de contenido
- *  @param pathRanking: dirección con los datos de ranking
+ /** Método crear el dataframe a partir del cual se le recomienda a los usuarios
+ *  @param pathRanking: dirección del archivo con los datos de ranking
  *  (descargado de https://grouplens.org/datasets/movielens/latest/)
  *  Los datos tienen la siguiente estructura
  *   _______________________________
  *  |userId|movieId|rating|timestamp|
  *  |1     |31     |2.5   |4512212  |
  *  |______|_______|______|_________|
+ *  @param pathMovies: dirección del archivo con los datos de las peliculas
  *  @param spark: SparkSession
  *  @param pathSave: dirección donde se guardara el dataframe
  *  @return no retorna nada, el método debe de guardar el dataframe preprocesado en el HDFS
  */
-  def preprocess(pathRanking: String, spark: SparkSession, pathSave: String) {
-    
+  def createMatrizForRecommendMovies(
+    pathRanking: String,
+    pathMovies: String,
+    spark: SparkSession): DataFrame = {
+      val rankingDf = spark.read.load(pathRanking)
+      val moviesDf = spark.read.load(pathMovies)
+      val normalizedDf = normalized(rankingDf)
+      val lshDf = lsh(normalizedDf, moviesDf, spark)
+      lshDf
   }
 
   /** Método encargado de normalizar los datos
-  *  @param path: direccion donde esta el archivo que almacena los datos
-  *  @param spark: SparkSession
+  *  @param path: datframe con os rankings de las peliculas
   *  @return dataframe con los datos normalizados
   */
-  def normalize(path: String, spark: SparkSession): DataFrame = {
-    val df = spark.read.format("text").load(path).select(expr("(split(value, ','))[0]").cast("string").as(Constants.COL_USER_ID), expr("(split(value, ','))[1]").cast("string").as(Constants.COL_MOVIE_ID), expr("(split(value, ','))[2]").cast("double").as(Constants.COL_RATING))
+  def normalized(rankingDf: DataFrame): DataFrame = {
+    val df = rankingDf.select(expr("(split('value', ','))[0]").cast("string").as(Constants.COL_USER_ID), expr("(split('value', ','))[1]").cast("string").as(Constants.COL_MOVIE_ID), expr("(split('value', ','))[2]").cast("double").as(Constants.COL_RATING))
     val dfxUser = df.groupBy(Constants.COL_USER_ID).agg(avg(df(Constants.COL_RATING)).as("promedio"))
     df.join(dfxUser,Constants.COL_USER_ID).withColumn(Constants.COL_RATING,df(Constants.COL_RATING)-dfxUser("promedio")).drop(df(Constants.COL_RATING)).drop(dfxUser("promedio"))
   }
 
-
- //////////////////////////ESTO ES LO MIO///////////////////////////////////////
   /** Método encargado de asignar los usuario a cubetas
   *  @param df: Dataframe con los datos normalizados
   *  @param dfMovies: Dataframe de las peliculas
